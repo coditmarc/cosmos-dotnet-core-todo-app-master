95206a1d6113e140113758bce65587a5efe73e3c
coditmarc <60918531+coditmarc@users.noreply.github.com>
2020-09-16 12:27:12 +0200

    Add project files.

---
+++ Models/AzureExcept.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace todo.Models
+{
+    using Newtonsoft.Json;
+    public class AzureExcept
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "prio")]
+        public int Prio { get; set; }
+
+        [JsonProperty(PropertyName = "PartitionKey")]
+        public string PartitionKey { get; set; }
+
+        [JsonProperty(PropertyName = "active")]
+        public bool Active { get; set; }
+
+        [JsonProperty(PropertyName = "code")]
+        public int Code { get; set; }
+
+        [JsonProperty(PropertyName = "result")]
+        public string Result { get; set; }
+
+        [JsonProperty(PropertyName = "context")]
+        public string Context { get; set; }
+
+        [JsonProperty(PropertyName = "returnCode")]
+        public int ReturnCode { get; set; }
+
+        [JsonProperty(PropertyName = "returnResult")]
+        public string ReturnResult { get; set; }
+
+        [JsonProperty(PropertyName = "returnDetails")]
+        public string ReturnDetails { get; set; }
+
+        [JsonProperty(PropertyName = "returnType")]
+        public string ReturnType { get; set; }
+
+    }
+}
